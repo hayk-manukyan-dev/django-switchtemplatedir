0ee75fc7b4ad672ab96ff7d50e6cbbe6a81bd70e
haykhman <haykhman@gmail.com>
2018-09-07 16:53:36 +0400

    init

--- TemplateDirManager.py
+++ TemplateDirManager.py
@@ -22,7 +22,6 @@ class GetTemplateDir(object):
     def switch(self, request):
         if template_dir in template_dir_settings:
             return template_dir_settings[template_dir]
-        print('+++++++  ' + str(template_dir))
         raise ValueError('TemplateDirSwitcher >> switcher was not registered in settings.TEMPLATE_DIR_SETTINGS')
 
     def templateDir(self, request):
@@ -36,21 +35,14 @@ class TemplateDirMiddleware:
 
     def __call__(self, request):
         template_dir = GetTemplateDir().templateDir(request)
-        print('--1--')
-#        try:
-        print('--2--')
-        response = self.get_response(request)
-        if type(template_dir) == list and len(template_dir) > 0:
-            print('if')
-            response.template_name = [response.template_name[0].split('.')[0] + template_dir[1]]
-            print(response.template_name[0])
-        response.template_name =  [template_dir[0] + str(response.template_name[0])]
-        print(response.template_name)
-        response.content = response.rendered_content
-        print('--3--')
-        return response
-#        except:
-#            print('--4--')
-#            return self.get_response(request)
+        try:
+            response = self.get_response(request)
+            if type(template_dir) == list and len(template_dir) > 0:
+                response.template_name = [response.template_name[0].split('.')[0] + template_dir[1]]
+            response.template_name =  [template_dir[0] + str(response.template_name[0])]
+            response.content = response.rendered_content
+            return response
+        except:
+            return self.get_response(request)
 
 
